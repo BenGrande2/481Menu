fbbc324616345da930bc2e2ac68ec7030d0a9bb7
BenGrande2 <benjamin.grande2@ucalgary.ca>
2017-03-03 18:43:30 -0700

    Add files via upload

---
+++ PostoPizza/PostoPizza/Navigation.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PostoPizza
+{
+    /// <summary>
+    /// Interaction logic for Navigation.xaml
+    /// </summary>
+    public partial class Navigation : Page
+    {
+        public Navigation()
+        {
+            InitializeComponent();
+            tabControl.SelectedIndex = 2;
+            
+        }
+
+        private void resizeTabs(object sender, SizeChangedEventArgs e)
+        {
+            var quarterWidth = (tabControl.ActualWidth / 4)-1.5;
+            var tenthHeight = (tabControl.ActualHeight / 20);
+            foodTab.Width = quarterWidth;
+            foodTab.Height = tenthHeight;
+            foodE.Width = quarterWidth*2;
+            foodE.Height = 3 * tenthHeight;
+            foodE.Margin = new Thickness(-quarterWidth, -tenthHeight*1.6 , 0, 0);
+
+           
+
+            drinkTab.Width = tabControl.ActualWidth / 4;
+            drinkE.Width = quarterWidth+4;
+            drinkE.Height = 3 * tenthHeight;
+            drinkE.Margin = new Thickness(quarterWidth-2, -tenthHeight * 1.6, 0, 0);
+
+            specTab.Width = quarterWidth;
+            specE.Width = quarterWidth+4;
+            specE.Height = 3 * tenthHeight;
+            specE.Margin = new Thickness(2*quarterWidth - 2, -tenthHeight * 1.6, 0, 0);
+
+            orderTab.Width = quarterWidth;
+            orderE.Width = quarterWidth * 2;
+            orderE.Height = 3 * tenthHeight;
+            orderE.Margin = new Thickness(quarterWidth*3, -tenthHeight * 1.6, 0, 0);
+
+
+
+        }
+    }
+}
---
+++ PostoPizza/PostoPizza/Specials.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PostoPizza
+{
+    /// <summary>
+    /// Interaction logic for Specials.xaml
+    /// </summary>
+    public partial class Specials : Page
+    {
+        string[][] specials = new string[][] {
+            new string[] {"Monday", "Industry Night"},
+            new string[] {"Tuesday", "Comfort food. Think Sunday dinner on a Tuesday night.\n Bottles of Lambrusco for $25!"},
+            new string[] {"Wednesday", "Date Night: Join us for a tasting menu for $35 per person. Feature $30 bottle of wine."},
+            new string[] {"Thursday", "1/2 price cocktails. $6 for two ounce cocktails."},
+            new string[] {"Friday", "Glasses of Sam's house-made Sangria for $8! Jugs for $35."},
+            new string[] {"Saturday", "15% off tasting menus"},
+            new string[] {"Sunday", "1/2 price pizza"}
+        };
+        public Specials()
+        {
+            InitializeComponent();
+            for (int i = 0; i < specials.Length; i++)
+            {
+                SpecialItem it = new SpecialItem();
+                it.Title.Content = specials[i][0];
+                it.Description.Text = specials[i][1];
+                specialsList.Children.Add(it);
+            }
+        }
+        private void resize(object sender, SizeChangedEventArgs e)
+        {
+            leftScroller.Width = this.ActualWidth / 2;
+            rightScroller.Width = this.ActualWidth / 2;
+            if (this.ActualHeight > 500)
+            {
+                for (int i = 0; i < specials.Length; i++)
+                {
+                    SpecialItem it = specialsList.Children[i] as SpecialItem;
+                    //it.Height = this.ActualHeight / specials.Length;
+                    it.Title.FontSize = this.ActualHeight * 0.04;
+                    it.Title.Padding = new Thickness(this.ActualHeight * 0.01);
+                    it.Description.FontSize = this.ActualHeight * 0.03;
+                    it.Description.Padding = new Thickness(this.ActualHeight * 0.01);
+
+                }
+            }
+            
+        }
+    }
+}
