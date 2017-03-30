b4ab1efabbf1398459d413559770ab1ccd0f54bb
Michal Widomski <mwidomsk@gmail.com>
2017-03-29 22:57:03 -0600

    Orderlist price updating and sending to kitchen button working

--- PostoPizza/PostoPizza/OrderList.xaml.cs
+++ PostoPizza/PostoPizza/OrderList.xaml.cs
@@ -21,40 +21,89 @@ namespace PostoPizza
     public partial class OrderList : UserControl
     {
         
-        private MenuItem[] alreadyOrderedList = { new MenuItem("Test Item", 43.00), new MenuItem("Pizza", 32.00), new MenuItem("Beer", 5.50) };
-        private MenuItem[] notOrderedList = { new MenuItem("Test Item2", 44.00), new MenuItem("Wine", 36.00), new MenuItem("Bread Appetizer", 12.00) };
+        //private MenuItem[] alreadyOrderedList = { new MenuItem("Test Item", 43.00), new MenuItem("Pizza", 32.00), new MenuItem("Beer", 5.50) };
+        //private MenuItem[] notOrderedList = { new MenuItem("Test Item2", 44.00), new MenuItem("Wine", 36.00), new MenuItem("Bread Appetizer", 12.00) };
+        private List<MenuItem> alreadyOrderedList = new List<MenuItem>();
+        private List<MenuItem> notOrderedList = new List<MenuItem>();
+
+        private double _price = 0;
 
         public OrderList()
         {
             InitializeComponent();
-            
-            for (int i = 0; i < alreadyOrderedList.Length; i++)
+            updateDisplay(true, null);
+        }
+
+        private void updateDisplay(bool clear, MenuItem item)
+        {
+            //clear NotOrderedList only if kitchen button has been pressed
+            if (clear == true)
             {
-                OrderMenuItem or = new OrderMenuItem();
-                or.menuItem = alreadyOrderedList[i];
-                AlreadyOrdered.Children.Add(or);
+                NotOrderedYet.Children.Clear();
+
+                for (int i = 0; i < alreadyOrderedList.Count; i++)
+                {
+                    OrderMenuItem ord = new OrderMenuItem();
+                    ord.menuItem = alreadyOrderedList[i];
+                    AlreadyOrdered.Children.Add(ord);
+                }
+
+                alreadyOrderedList.Clear();
+                notOrderedList.Clear();
+                this.total.Content = "Total: $" + Math.Round(_price, 2).ToString("0.00");
             }
-            for (int i = 0; i < notOrderedList.Length; i++)
+
+            OrderMenuItem or = new OrderMenuItem();
+            if (item != null)
             {
-                OrderMenuItem or = new OrderMenuItem();
-                or.menuItem = notOrderedList[i];
+                or.menuItem = item;
                 NotOrderedYet.Children.Add(or);
             }
+            //notOrderedList.Clear();
         }
 
         private void resizeOrderlist(object sender, SizeChangedEventArgs e)
         {
             orderListStack.Width = alreadyOrderedLabel.ActualWidth * 1.5;
-            for (int i = 0; i < alreadyOrderedList.Length; i++)
+            for (int i = 0; i < alreadyOrderedList.Count; i++)
             {
                 OrderMenuItem or = AlreadyOrdered.Children[i] as OrderMenuItem;
                 or.FontSize = this.ActualHeight * 0.03;
             }
-            for (int i = 0; i < notOrderedList.Length; i++)
+            for (int i = 0; i < notOrderedList.Count; i++)
             {
                 OrderMenuItem or = NotOrderedYet.Children[i] as OrderMenuItem;
                 or.FontSize = this.ActualHeight * 0.03;
             }
+        }
+
+        /// <summary>
+        /// Takes in each item to be added to notOrderedList
+        /// </summary>
+        /// <param name="item"></param>
+        public void addItem(MenuItem item)
+        {
+            //add item to 
+            notOrderedList.Add(item);
+
+            //update price
+            _price += item.Price;
+
+            //update display
+            updateDisplay(false, item);
+        }
+
+        /// <summary>
+        /// Called by event listener for send to kitchen button. Updates total bill and clears Not ordered display
+        /// </summary>
+        public void sendOrder()
+        {
+            for(int i = 0; i < notOrderedList.Count; i++)
+            {
+                alreadyOrderedList.Add(notOrderedList[i]);
+            }
+
+            updateDisplay(true, null);
         }
     }
 }
